d958ba589dac66d8df478d2a52e8bc778663eb3e
AidenFstr <Ugrok3@mail.ru>
2024-04-11 08:10:11 +0300

    Добавил проверку на использование address-mask без address-start. Пофиксил отображение диапазона адресов в отчете.

--- EffectiveMobile_test/Program.cs
+++ EffectiveMobile_test/Program.cs
@@ -82,8 +82,8 @@ namespace EffectiveMobile_test
         public string AddressStart { get; set; }
 
         [Option('m',"address-mask", Required = false, Default = "0.0.0.0", HelpText = "верхнаяя граница диапазона адресов, " +
-            " необязательный параметр, по умолчанию обрабатываются все адреса" +
-            " Параметр нельзя использовать, если не задан address-start")]
+                                                                                      " необязательный параметр, по умолчанию обрабатываются все адреса" +
+                                                                                      " Параметр нельзя использовать, если не задан address-start")]
         public string AddressMask { get; set; }
 
         [Option("time-start", Required = true, HelpText = "нижняя граница временного интервала\r\n")]
@@ -136,6 +136,10 @@ namespace EffectiveMobile_test
                 DateTime TimeStart = DateConverter.ConvertDate(opts.TimeStart);
                 DateTime TimeEnd = DateConverter.ConvertDate(opts.TimeEnd);
 
+                // Проверяем IP адрес и маску
+                if (opts.AddressStart == null && opts.AddressMask != null) { Console.WriteLine("    Ошибка: Нельзя использовать параметр 'm, address-mask' без 's, adress-start'!"); return; }
+
+
                 List<LogEntry> logEntries = new List<LogEntry>(); 
                 using (StreamReader reader = new StreamReader(opts.LogFile))
 
@@ -155,12 +159,12 @@ namespace EffectiveMobile_test
 
                 var filteredEntries = logEntries.Where(e => e.Time >= TimeStart && e.Time <= TimeEnd).GroupBy(e => e.IP).Select(g => new { IP = g.Key, Count = g.Count() });
 
-                if (opts.AddressStart != null)
+                if (IpAddressValidator.IsValidIpAddress(opts.AddressStart))
                 {
                     // Сортируем по IP
                     var sortedByIP = filteredEntries.OrderBy(e => e.IP).ToList();
 
-                    if (IpAddressValidator.IsValidIpAddress(opts.AddressStart) && IpAddressValidator.IsValidIpAddress(opts.AddressMask))
+                    if (IpAddressValidator.IsValidIpAddress(opts.AddressMask))
                     {
                         // фильтуем от AddressStart до AddressMask
                         var filteredByIp = sortedByIP.Where(e => IsInSubnet(IPAddress.Parse(e.IP), IPAddress.Parse(opts.AddressStart), IPAddress.Parse(opts.AddressMask)));
@@ -181,8 +185,9 @@ namespace EffectiveMobile_test
 
                 sw.Stop();
 
+                // Выводим отчет
                 Console.WriteLine($"\nВременной интервал: {TimeStart} - {TimeEnd}");
-                Console.WriteLine($"Диапазон адресов: {opts.AddressStart} - {opts.AddressMask}");
+                Console.WriteLine($"Диапазон адресов: {IPAddress.Parse(opts.AddressStart)} - {IPAddress.Parse(opts.AddressMask)}");
                 Console.WriteLine($"        Совпадений: {filteredEntries.Count()}");
                 Console.WriteLine($"  Время выполнения: {sw.ElapsedMilliseconds} мс");
                 Console.WriteLine(" Результат сохранен в {0}", opts.OutputFile);
